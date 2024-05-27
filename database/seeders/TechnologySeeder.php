e16e559737c06fb8b58f9cd9b51f2f86d70e86ae
chrialge <chrialge@gmail.com>
2024-05-27 12:49:53 +0200

    add model, migration and seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $technologies = ['html', 'css', 'js', 'php', 'vue', 'blade', 'laravel', 'sass'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology, '-');
+            $newTechnology->save();
+        }
+    }
+}
